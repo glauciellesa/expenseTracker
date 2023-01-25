265976fee00617e391b7b106cd8f54ddf80cd4ca
Glaucielle Sá <glauciellesa@gmail.com>
2023-01-25 11:04:43 +0100

    Submit handler logic

--- src/components/newExpense/ExpenseForm.jsx
+++ src/components/newExpense/ExpenseForm.jsx
@@ -1,4 +1,82 @@
+import { useState } from "react";
 import styled from "styled-components";
+
+const ExpenseForm = () => {
+  const [entiredTitle, setEntiredTitle] = useState("");
+  const [entiredAmount, setEntiredAmount] = useState("");
+  const [entiredDate, setEntiredDate] = useState("");
+  /* const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmout: "",
+    enteredDate: "",
+  }); */
+  const titleChangeHandler = (event) => {
+    setEntiredTitle(event.target.value);
+    /* setUserInput(() => {
+      return {
+        ...userInput,
+        enteredTitle: event.target.value,
+      };
+    }); */
+  };
+  const amountChangeHandler = (event) => {
+    setEntiredAmount(event.target.value);
+    /* setUserInput({
+      ...userInput,
+      enteredAmout: event.target.value,
+    }); it's not a good practice*/
+    /* setUserInput(() => {
+      return {
+        ...userInput,
+        enteredAmout: event.target.value,
+      };
+    }); */
+  };
+  const dateChangeHandler = (event) => {
+    setEntiredDate(event.target.value);
+    /* setUserInput(() => {
+      return {
+        ...userInput,
+        enteredDate: event.target.value,
+      };
+    }); */
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault(); // will prevent the page from reloading once the button is clicked
+    const expenseData = {
+      title: entiredTitle,
+      amount: entiredAmount,
+      data: new Date(entiredDate), //As entiredData is a string I'll need to convert it to a objetct using the constroctur new Data()
+    };
+    console.log(expenseData);
+  };
+  return (
+    <StyledExpenseForm>
+      <form onSubmit={submitHandler}>
+        <div className="newExpense-container">
+          <div className="newExpense-item">
+            <label htmlFor="title">Title</label>
+            <input type="text" onChange={titleChangeHandler} />
+          </div>
+          <div className="newExpense-item">
+            <label htmlFor="title">Amount</label>
+            <input type="string" onChange={amountChangeHandler} />
+          </div>
+          <div className="newExpense-item">
+            <label htmlFor="title">Date</label>
+            <input type="date" onChange={dateChangeHandler} />
+          </div>
+        </div>
+        <div className="newExpense-action">
+          <button type="submit">Add Expense</button>
+        </div>
+      </form>
+    </StyledExpenseForm>
+  );
+};
+
+export default ExpenseForm;
 
 const StyledExpenseForm = styled.div`
   .newExpense-container {
@@ -43,40 +121,3 @@ const StyledExpenseForm = styled.div`
     background-color: #a323e8;
   }
 `;
-
-const ExpenseForm = () => {
-  const titleChangeHandler = (event) => {
-    console.log(event.target.value);
-  };
-  const amountChangeHandler = (event) => {
-    console.log(event.target.value);
-  };
-  const dateChangeHandler = (event) => {
-    console.log(event.target.value);
-  };
-  return (
-    <StyledExpenseForm>
-      <form>
-        <div className="newExpense-container">
-          <div className="newExpense-item">
-            <label htmlFor="title">Title</label>
-            <input type="text" onChange={titleChangeHandler} />
-          </div>
-          <div className="newExpense-item">
-            <label htmlFor="title">Amount</label>
-            <input type="number" onChange={amountChangeHandler} />
-          </div>
-          <div className="newExpense-item">
-            <label htmlFor="title">Date</label>
-            <input type="date" onChange={dateChangeHandler} />
-          </div>
-        </div>
-        <div className="newExpense-action">
-          <button type="submit">Add Expense</button>
-        </div>
-      </form>
-    </StyledExpenseForm>
-  );
-};
-
-export default ExpenseForm;
